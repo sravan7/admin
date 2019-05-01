bbab6e6ebe9fd9408d1c05d2995bce5282ad09bf
sravan7 <esd14i017@iiitdm.ac.in>
2019-05-01 06:33:17 +0530

    auth json

--- permission.php
+++ permission.php
@@ -3,9 +3,9 @@
     require_once __DIR__.'/vendor/autoload.php';
     ini_set(' session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT'])));
     $client = new Google_Client();
-    $client->setAuthConfigFile('client_secret_737091014820-6vtpr3l3jrtfjp434n0u5ntqhcvei9ir.apps.googleusercontent.com.json');
+    $client->setAuthConfigFile('client_secret_303235166535-nfblp1roplc8ku613gsm2to01g3cafjh.apps.googleusercontent.com.json');
     $client->setApplicationName("IIITDM Bonafide");
-    $client->setRedirectUri("http://".$_SERVER["HTTP_HOST"]."/index.php");
+    $client->setRedirectUri("https://".$_SERVER["HTTP_HOST"]."/index.php");
     $client->setHostedDomain("iiitdm.ac.in");
     $client->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
