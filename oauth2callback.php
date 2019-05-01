301d42ed24c248637dc0708d9922fdd82b9b4698
sravan7 <esd14i017@iiitdm.ac.in>
2019-05-01 07:23:05 +0530

    scopes

--- oauth2callback.php
+++ oauth2callback.php
@@ -6,8 +6,11 @@ session_start();
 
 $client = new Google_Client();
 $client->setAuthConfigFile('client_secret_303235166535-nfblp1roplc8ku613gsm2to01g3cafjh.apps.googleusercontent.com.json');
+$client->setAccessType("offline");        // offline access
+$client->setIncludeGrantedScopes(true);   // incremental auth
+$client->setHostedDomain("iiitdm.ac.in");
+$client->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
-
 if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
